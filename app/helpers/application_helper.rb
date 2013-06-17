0cc57c52580c0fddc02f27f4d398dd9acb0e1ef6
dreamlx <dreamlinx@gmail.com>
2013-06-18 02:46:55 +0800

    change style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def qiniu_image(post)
     if post.cost == 0 or (user_signed_in? and post.visited?(current_user.id || 0))
-      image_tag post.photo.url+"?imageView/2/h/200" unless post.photo.blank?
+      image_tag post.photo.url+"?imageView/1/h/200/w/300" unless post.photo.blank?
     else
-      image_tag post.photo.url+"?imageView/2/h/200/q/2" unless post.photo.blank?
+      image_tag post.photo.url+"?imageView/1/h/200/w/300/q/1" unless post.photo.blank?
     end
   end
 end
