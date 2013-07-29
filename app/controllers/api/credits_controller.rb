e51ceaa119aeb0bc52d2c62eb72b40223778709a
dreamlx <dreamlinx@gmail.com>
2013-07-29 22:50:42 +0800

    remove csrf

--- app/controllers/api/credits_controller.rb
+++ app/controllers/api/credits_controller.rb
@@ -1,4 +1,5 @@
 class Api::CreditsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   respond_to :json
   def show
     @credit = Credit.find(params[:id])
--- app/controllers/api/dailyposts_controller.rb
+++ app/controllers/api/dailyposts_controller.rb
@@ -1,5 +1,6 @@
 class Api::DailypostsController < ApplicationController
   before_filter :authenticate_user!
+  skip_before_filter :verify_authenticity_token
   respond_to :json
   def index
     @dailyposts = Dailypost.order(" updated_at DESC")
